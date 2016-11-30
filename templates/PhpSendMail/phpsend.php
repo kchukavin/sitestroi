d6906c71b6692f3142108a87e7179362d2b0ac5f
Konstantin <konst.site@gmail.com>
2016-11-30 11:52:53 +0300

    phpsend updated

--- templates/PhpSendMail/phpsend.php
+++ templates/PhpSendMail/phpsend.php
@@ -1,11 +1,14 @@
 <?php
 
+$redirectPath = '6520.html';
+
 function post2Msg($post) {
 	
 	$translate = array(
 		'name'		=> 'Имя',
 		'phone'		=> 'Телефон',
 		'email'		=> 'Email',
+		'phone_email'		=> 'Тел. или email',
 		'message'	=> 'Сообщение',
 		'date'		=> 'Дата',
 		'kolichestvo'	=> 'Количество людей',
@@ -28,8 +31,10 @@ function post2Msg($post) {
 	return $msg;
 }
 
+//$address = "zakaz@avtodvigatel.ru"; // Сюда впишите свою эл. почту
+//$address = "sitestroi-metrika-all@yandex.ru"; // Сюда впишите свою эл. почту
 $address = "konst.site@gmail.com"; // Сюда впишите свою эл. почту
-$sub = $_POST['form_name'] . ' <transport-akbars.ru>'; // Тема письма
+$sub = $_POST['form_name'] . ' <lpavtodvigatel.ru>'; // Тема письма
 $email = 'Заказ <no-reply@nodomain.no>'; // От кого
 
 // А здесь прописывается текст сообщения, \n - перенос строки
@@ -39,14 +44,14 @@ $message = post2Msg($_POST);
 $send = mail ($address,$sub,$message,"Content-type:text/plain; charset = utf-8\r\nFrom:$email");
 
 ini_set('short_open_tag', 'On');
-header('Refresh: 3; URL=index.html');
+header('Refresh: 3; URL='.$redirectPath);
 
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<meta http-equiv="refresh" content="3; url=index.html">
+<meta http-equiv="refresh" content="3; url=<?php echo $redirectPath;?>">
 
 <title>С вами свяжутся</title>
 
@@ -58,7 +63,7 @@ body {
 </style>
 
 <script type="text/javascript">
-setTimeout('location.replace("/index.html")', 3000);
+setTimeout('location.replace("<?php echo $redirectPath;?>")', 2000);
 /*Изменить текущий адрес страницы через 3 секунды (3000 миллисекунд)*/
 </script> 
 </head>
