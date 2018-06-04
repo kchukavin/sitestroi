e77d6d85492568287317645111d18a9774877bc1
Konstantin C <konst.site@gmail.com>
2018-06-04 09:51:47 +0300

    SCSS updated

--- diafan/scssphp/bin/scss.php
+++ diafan/scssphp/bin/scss.php
@@ -4,6 +4,10 @@ require __DIR__ . '/../vendor/leafo/scssphp/example/Server.php';
 
 use Leafo\ScssPhp\Compiler;
 
+// CONFIG BEGIN
+
+$LOGS = true;
+
 $customDir = __DIR__ . '/../custom/tatartdom2017';
 
 $tasks = array(
@@ -19,13 +23,52 @@ $tasks = array(
 	),
 );
 
+// CONFIG END
+
+writeLog('Processing:<br>');
 
 foreach ($tasks as $task) {
+	writeLog($task['import_dir'] . ' => ' . $task['output_file'] . "... ");
+	$oldHash = loadHash($task['output_file'] . '.hash');
+	$newHash = calcFilesHash($task['import_dir']);
+	
+	if ($oldHash === $newHash) {
+		writeLog("No changes.\n");
+		continue;
+	}
+	
+	saveHash($task['output_file'] . '.hash', $newHash);
+	
 	$scss = new Compiler();
 	$scss->setImportPaths($task['import_dir']);
 
-	echo 'Compiling... ';
+	writeLog('Compiling... ');
 	file_put_contents($task['output_file'], $scss->compile('@import "'. $task['import_file'] .'";'));
-	echo $task['output_file'] . ' done.<br>';
+	writeLog(' done.<br>');
 }
 
+
+function loadHash($filename)
+{
+	if (!file_exists($filename)) return '';
+	return file_get_contents($filename);
+}
+
+function saveHash($filename, $hash)
+{
+	return file_put_contents($filename, $hash);
+}
+
+function calcFilesHash($dir)
+{
+	$data = '';
+	foreach (scandir($dir) as $filename) {
+		$data .= filemtime($dir . '/' . $filename);
+	}
+	return md5($data);
+}
+
+function writeLog($msg)
+{
+	if ($LOGS) echo $msg;
+}
